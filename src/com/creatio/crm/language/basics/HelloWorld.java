a7e2d35936af4aec91b71645f3942d64f3d05323
mail2fameeda <mail2fameeda@gmail.com>
2024-12-22 19:10:26 +1100

    Assignment-2 Arrays

--- src/com/creatio/crm/language/basics/HelloWorld.java
+++ src/com/creatio/crm/language/basics/HelloWorld.java
@@ -8,85 +8,110 @@ public class HelloWorld {
 		
 		System.out.println("Hello World");
 			
+		/* This is a Primitive data type -Float */
 		float degree=25.5f;
 		
 		System.out.println("Temperature of a city in degrees Celsius is :" +degree);
 		
+		/* This is a Primitive data type -Boolean */
 		boolean order= true ;
 		
 		System.out.println("Whether a customer has placed an order is:" +order);
 		
+		/* This is a Primitive data type - integer */
+				
 		int phone = 1234567890;
 		
 		System.out.println("Person's phone number is :"+phone);
+		
+		/* This is a Primitive data type - float*/
 		
 		float Account=1000.50f;
 		
 		System.out.println("Amount of money in a customer's bank account is :"+Account);
 		
+		/* This is a Non Primitive data type - String */
 		String email ="john.doe@example.com";
 		
 		System.out.println("Person's email address is :" +email);
+		
+		/* This is a Primitive data type - double*/
 		
 		double location=(37.7749 -122.4194);
 		
 		System.out.println("Coordinates of a location (latitude, longitude)is :" +location);
 		
+		/* This is a Primitive data type - boolean */
 		boolean status= false;
 		
 		System.out.println("Person's marital status is:" +status);
+		
+		/* This is a Non Primitive data type - String */
 		
 		String Occp ="Software Engineer";
 		
 		System.out.println("Person's occupation is :" +Occp);
 		
+		/* This is a Non Primitive data type - String */
+				
 		String color ="Blue";
 		
 		System.out.println("Person's favourite colour:" +color);
 		
-		
+		/* This is a Primitive data type - short */
 		short year=2024;
 		
 		System.out.println("Current year is:"+year);
+		
+		/* This is a Primitive data type - long */
 		
 		long followers = 1000000L;
 		
 		System.out.println("11.Number of followers on a social media platform is:" +followers);
 		
+		/* This is a Primitive data type - float */
+		
 		float rating=7.5f;
 		
 		System.out.println("Rating of a movie is :"+rating);
 		
-		char type='A';
+		/* This is a Primitive data type - Char */
+		
+		char type='O';
 		System.out.println("Person's blood type is:"+type);
+		
+		/* This is a Non Primitive data type - String */
 		
 		String book ="To Kill a Mockingbird";
 		
 		System.out.println("Title of a book is :" +book);
 		
+		/* This is a Primitive data type - Short */
+		
 		short emp=500;
 		
 		System.out.println("15.Number of employees in a company are" +emp);
 		
+		/* This is a Non Primitive data type - String */
 		
 		String event="2:30PM";
 		System.out.println("Time of an event is:"+event);
 		
-
+		/* This is a Non Primitive data type - String */
 		String eye="Brown";
 		System.out.println("Person's eye color is:"+eye);
 		
-
+		/* This is a Non Primitive data type - String */
+		
 		String country="United States";
 		System.out.println("Name of a country is :" +country);
 		
-		
+		/* This is a Non Primitive data type - String */
 		String place="New York City";
 		System.out.println("19.Person's birthplace is :"+place);
 		
-		
+		/* This is a Primitive data type - String */
 		float distance=200.5f;
-		
 		System.out.println("Distance between two cities is :"+distance);
 		
 		
