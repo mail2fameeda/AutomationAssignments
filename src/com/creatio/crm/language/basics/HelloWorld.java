69fd8c634619d72f24e00e7d6a85a60b85a6c207
mail2fameeda <mail2fameeda@gmail.com>
2024-12-22 16:30:28 +1100

    Assignment 1 - Java Data Types

---
+++ src/com/creatio/crm/language/basics/HelloWorld.java
@@ -0,0 +1,95 @@
+package com.creatio.crm.language.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args)
+	
+	{
+		
+		System.out.println("Hello World");
+			
+		float degree=25.5f;
+		
+		System.out.println("Temperature of a city in degrees Celsius is :" +degree);
+		
+		boolean order= true ;
+		
+		System.out.println("Whether a customer has placed an order is:" +order);
+		
+		int phone = 1234567890;
+		
+		System.out.println("Person's phone number is :"+phone);
+		
+		float Account=1000.50f;
+		
+		System.out.println("Amount of money in a customer's bank account is :"+Account);
+		
+		String email ="john.doe@example.com";
+		
+		System.out.println("Person's email address is :" +email);
+		
+		double location=(37.7749 -122.4194);
+		
+		System.out.println("Coordinates of a location (latitude, longitude)is :" +location);
+		
+		boolean status= false;
+		
+		System.out.println("Person's marital status is:" +status);
+		
+		String Occp ="Software Engineer";
+		
+		System.out.println("Person's occupation is :" +Occp);
+		
+		String color ="Blue";
+		
+		System.out.println("Person's favourite colour:" +color);
+		
+		
+		short year=2024;
+		
+		System.out.println("Current year is:"+year);
+		
+		long followers = 1000000L;
+		
+		System.out.println("11.Number of followers on a social media platform is:" +followers);
+		
+		float rating=7.5f;
+		
+		System.out.println("Rating of a movie is :"+rating);
+		
+		char type='A';
+		System.out.println("Person's blood type is:"+type);
+		
+		String book ="To Kill a Mockingbird";
+		
+		System.out.println("Title of a book is :" +book);
+		
+		short emp=500;
+		
+		System.out.println("15.Number of employees in a company are" +emp);
+		
+		
+		String event="2:30PM";
+		System.out.println("Time of an event is:"+event);
+		
+
+		String eye="Brown";
+		System.out.println("Person's eye color is:"+eye);
+		
+
+		String country="United States";
+		System.out.println("Name of a country is :" +country);
+		
+		
+		String place="New York City";
+		System.out.println("19.Person's birthplace is :"+place);
+		
+		
+		float distance=200.5f;
+		
+		System.out.println("Distance between two cities is :"+distance);
+		
+		
+	}
+
+}
